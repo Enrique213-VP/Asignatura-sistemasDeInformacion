96f1250f588e28eda24e095e6addddb18591e493
Sergio Vargas <70002378+Enrique213-VP@users.noreply.github.com>
2025-10-14 15:44:16 -0500

    code class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const ATTENDANCE_CONFIG = {
-    validCodes: ['YUCA', 'API'],
+    validCodes: ['CLOUD', 'API'],
     excelUrl: 'https://docs.google.com/spreadsheets/d/1WQ4HWhgxy5VIyP_l3ylDl-Ka1pSjB8XeiIF6opmDXjM/edit?usp=sharing'
 };
 
