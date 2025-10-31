82ef74c7e9f061f047b53643a3ab160b1c5b7a65
Sergio Vargas <70002378+Enrique213-VP@users.noreply.github.com>
2025-10-31 00:22:11 -0500

    Second send

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const ATTENDANCE_CONFIG = {
-    validCodes: ['COPNIA', 'UNAL'],
+    validCodes: ['CASSANDRA', 'CUY'],
     excelUrl: 'https://docs.google.com/spreadsheets/d/1WQ4HWhgxy5VIyP_l3ylDl-Ka1pSjB8XeiIF6opmDXjM/edit?usp=sharing'
 };
 
